65ab383e080cbc277da330c7c39f88e0ac60f84f
mayuriv-webonise <mayuri.vibhute@webonise.com>
2023-07-25 10:26:59 +0530

    yup validation chnages

--- my-react-practice-app/src/components/SortingHOC.js
+++ my-react-practice-app/src/components/SortingHOC.js
@@ -5,7 +5,7 @@ const withSorting = (WrappedComponent) => {
     const [updatedItems, setUpdatedItems] = useState(items);
 
     // Memoized function to increment the count of each item
-    const incrementCount = (id) => {
+    const incrementCount = useCallback((id) => {
       // Implement your logic here to increment the count of the item with the given id
       const updatedItemsCopy = updatedItems.map((item) =>
         item.id === id ? { ...item, count: item.count + 1 } : item
@@ -13,7 +13,7 @@ const withSorting = (WrappedComponent) => {
       setUpdatedItems(updatedItemsCopy);
       // Call the logic to update the state or perform other actions with the updatedItemsCopy array
       console.log(`Incrementing count for item with id: ${id}`);
-    };
+    }, [updatedItems]);
 
     // Ensure updatedItems is an array before sorting
     const sortedItems = useMemo(() => {
